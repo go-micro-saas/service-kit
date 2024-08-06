a985669e8ea10f3c817925e289591de4d5e2a11a
chenkaiguang <chenkaiguang@realibox.com>
2024-08-06 10:27:32 +0800

    feat: export services

--- testdata/ping-service/cmd/ping-service/export/export.go
+++ testdata/ping-service/cmd/ping-service/export/export.go
@@ -1,9 +1,12 @@
 package pingexport
 
 import (
+	"github.com/go-kratos/kratos/v2/transport/grpc"
+	"github.com/go-kratos/kratos/v2/transport/http"
+	serverutil "github.com/go-micro-saas/service-kit/server"
 	setuputil "github.com/go-micro-saas/service-kit/setup"
 )
 
-func Export(launcherManager setuputil.LauncherManager) error {
-	return nil
+func Export(launcherManager setuputil.LauncherManager, hs *http.Server, gs *grpc.Server) (*serverutil.Services, func(), error) {
+	return initServices(launcherManager, hs, gs)
 }
