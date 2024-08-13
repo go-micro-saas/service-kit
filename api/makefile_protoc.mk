6ada3257f788dab1c897169462cf639c30484ad4
ikaiguang <ikaiguang@163.com>
2024-08-13 22:21:36 +0800

    chore: change makefile

--- api/makefile_protoc.mk
+++ api/makefile_protoc.mk
@@ -1,5 +1,9 @@
+override ABSOLUTE_MAKEFILE := $(abspath $(lastword $(MAKEFILE_LIST)))
+override ABSOLUTE_PATH := $(patsubst %/,%,$(dir $(ABSOLUTE_MAKEFILE)))
+override REL_PROJECT_PATH := $(subst $(PROJECT_ABS_PATH)/,,$(ABSOLUTE_PATH))
+
 # saas services
-SAAS_SERVICE_PROTO_FILES=$(shell cd $(PROJECT_PATH) && find api -name "*.proto")
+SAAS_SERVICE_PROTO_FILES := $(shell find ./$(REL_PROJECT_PATH) -name "*.proto")
 .PHONY: protoc-api-protobuf
 # protoc :-->: generate services api protobuf
 protoc-api-protobuf:
@@ -7,9 +11,10 @@ protoc-api-protobuf:
 	$(call protoc_protobuf,$(SAAS_SERVICE_PROTO_FILES))
 
 # specified server
-SAAS_SERVICE_SPECIFIED_FILES=$(shell cd $(PROJECT_PATH) && find ./api/${service} -name "*.proto")
+SAAS_SERVICE_SPECIFIED_FILES := $(shell find ./$(REL_PROJECT_PATH)/${service} -name "*.proto")
 .PHONY: protoc-specified-api
 # protoc :-->: example: make protoc-specified-api service=ping-service
 protoc-specified-api:
 	@echo "# generate ${service} protobuf"
 	$(call protoc_protobuf,$(SAAS_SERVICE_SPECIFIED_FILES))
+
