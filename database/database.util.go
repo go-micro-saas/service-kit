391032d10d699c004ad33340737f16583c401b40
ikaiguang <ikaiguang@163.com>
2024-09-08 11:15:33 +0800

    feat: 添加更多的数据库迁移工具

--- database/database.util.go
+++ database/database.util.go
@@ -1,16 +1,32 @@
 package dbutil
 
-import setuputil "github.com/go-micro-saas/service-kit/setup"
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	setuputil "github.com/go-micro-saas/service-kit/setup"
+)
 
 type MigrationFunc func(launcherManager setuputil.LauncherManager, opts ...MigrationOptions)
 
 // MigrationOptions ...
 type MigrationOptions struct {
-	Close bool
+	Logger log.Logger
+	Close  bool
+}
+
+func DefaultMigrationOptions() *MigrationOptions {
+	return &MigrationOptions{
+		Logger: log.DefaultLogger,
+	}
 }
 
 // MigrationOption ...
 type MigrationOption func(*MigrationOptions)
+
+func WithLogger(logger log.Logger) MigrationOption {
+	return func(o *MigrationOptions) {
+		o.Logger = logger
+	}
+}
 
 // WithClose 退出后关闭资源
 func WithClose() MigrationOption {
