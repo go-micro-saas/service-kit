c1d7b0b6f5a1328cbd4837eb2750bf3e0978a61f
chenkaiguang <chenkaiguang@realibox.com>
2024-10-08 17:05:57 +0800

    feat: dockerfile

--- testdata/ping-service/devops/makefile_cicd.mk
+++ testdata/ping-service/devops/makefile_cicd.mk
@@ -3,10 +3,14 @@
 # build :-->: service image
 build:
 	#docker build -t ping-service -f ./testdata/ping-service/devops/Dockerfile .
+	#docker pull golang:1.22.8
+	#docker pull debian:stable-20240926-slim
 	docker build \
-    		--build-arg APP_DIR=testdata \
-    		--build-arg SERVICE_NAME=ping-service \
-    		--build-arg VERSION=v1.0.1 \
-    		-t ping-service:v1.0.1 \
-    		-f ./testdata/ping-service/devops/Dockerfile .
+		--build-arg BUILD_FROM_IMAGE=golang:1.22.8 \
+		--build-arg RUN_SERVICE_IMAGE=debian:stable-20240926-slim \
+		--build-arg APP_DIR=testdata \
+		--build-arg SERVICE_NAME=ping-service \
+		--build-arg VERSION=latest \
+		-t ping-service:latest \
+		-f ./testdata/ping-service/devops/Dockerfile .
 
