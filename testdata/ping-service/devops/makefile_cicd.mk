53fd68e68154f09a163e45b72527c8f997d5532e
chenkaiguang <chenkaiguang@realibox.com>
2024-10-08 16:44:39 +0800

    feat: dockerfile

--- testdata/ping-service/devops/makefile_cicd.mk
+++ testdata/ping-service/devops/makefile_cicd.mk
@@ -2,5 +2,11 @@
 .PHONY: build
 # build :-->: service image
 build:
-	docker build -t ping-service -f ./devops/Dockerfile .
+	#docker build -t ping-service -f ./testdata/ping-service/devops/Dockerfile .
+	docker build \
+    		--build-arg APP_DIR=testdata \
+    		--build-arg SERVICE_NAME=ping-service \
+    		--build-arg VERSION=v1.0.1 \
+    		-t ping-service:v1.0.1 \
+    		-f ./testdata/ping-service/devops/Dockerfile .
 
